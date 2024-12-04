62a6ff5fb44b175ba39a47d5bd35b50efb743cf7
Kerim <160826772+Kerim91@users.noreply.github.com>
2024-12-04 23:58:09 +0300

    Update do_you_see.sh

--- do_you_see.sh
+++ do_you_see.sh
@@ -1,12 +1,31 @@
-                                            
+
 #!/bin/bash
+
 
 #  gizli kısım  için
 echo "I am waiting..."
 read -s klavye_vuruslari
 
-# Yazıyı bir dosyaya kaydet,var olan bilgilerin ise üstüne...
-echo "$klavye_vuruslari" >> your_art.txt
 
-echo "done"
+#admin tavsiyesi değildir
+# Girişin boş olmadığını kontrol edin
+#if [ -z "$klavye_vuruslari" ]; then
+ #   echo "Giriş boş olamaz!"
+  #  exit 1
+#fi
+
+
+
+# Tarihi alın
+current_date=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Girdiyi şifreleme (modern PBKDF2 yöntemi ile)
+# "yourpassword" kısmını kendi belirlediğiniz güçlü bir şifre ile değiştirin,default bırakma riskli anlatmaya gerek yok sanırım...
+encrypted_input=$(echo "$klavye_vuruslari" | openssl enc -aes-256-cbc -a -salt -pbkdf2 -pass pass:yourpassword)  #ufak bir openssl sıkıntısı es geçildi(şimdilik)
+
+# Tarihi ve şifreli girişi dosyaya yazma
+echo "$current_date - $encrypted_input" >> your_art.txt
+
+# İşlem tamamlandığında bilgilendirme
+echo echo "done!"
 
